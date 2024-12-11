998ab97c21d9dd165de2bb0db71492fb9666e05e
thdangtr <thdangtr@gmail.com>
2024-12-11 23:26:25 +0700

    setup brower with profile

--- src/app/test.py
+++ src/app/test.py
@@ -1,9 +1,30 @@
+import os
+import time
+
 from app.utils.logger import logger
-from app.utils.gsheet import worksheet
-from app.models.sheet_models import Product
+from app.utils.browser import BrwDriver, get_chrome_profile_path
+from selenium import webdriver
 
-p = Product.get(worksheet, index=5)
+options = webdriver.ChromeOptions()
 
-p.Seller = "Dang Tran"
-p.update()
-print(p.model_dump_json())
+profile_path = get_chrome_profile_path()
+print(profile_path)
+
+options.add_argument(f"user-data-dir={os.path.dirname(profile_path)}")
+options.add_argument("--profile-directory=Default")
+
+driver = BrwDriver(options=options)
+
+
+driver.get("https://google.com")
+print(driver.execute_script("return navigator.userAgent;"))
+time.sleep(1)
+driver.random_user_agent()
+driver.get("https://codeforces.com")
+print(driver.execute_script("return navigator.userAgent;"))
+driver.random_user_agent()
+print(driver.execute_script("return navigator.userAgent;"))
+
+print(get_chrome_profile_path())
+print("sleeping")
+time.sleep(1000)
